ae17f745d953dc32d46bbd349b8f7be185b34220
Nobaliz <72144645+Nobaliz@users.noreply.github.com>
2025-12-01 17:36:21 -0500

    inicial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,13 @@
 import { createRoot } from 'react-dom/client'
+import * as Sentry from '@sentry/react'
 import App from './App.tsx'
+import { initSentry } from './sentry'
+
+// Initialize Sentry before rendering the app
+initSentry()
+
+const SentryApp = Sentry.withProfiler(App)
 
 createRoot(document.getElementById('root')!).render(
-  <App />
+  <SentryApp />
 )
