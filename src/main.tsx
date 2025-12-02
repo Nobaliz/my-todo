876da4376206c90c434a9d3fe8fb1d3944315bd3
Nobaliz <72144645+Nobaliz@users.noreply.github.com>
2025-12-01 22:07:13 -0500

    Fix React build configuration and Firebase deploy

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,13 @@ import { createRoot } from 'react-dom/client'
 import * as Sentry from '@sentry/react'
 import App from './App.tsx'
 import { initSentry } from './sentry'
+import { useEffect } from "react";
+import { initAnalytics } from "./analytics";
+
+useEffect(() => {
+  initAnalytics();
+}, []);
+
 
 // Initialize Sentry before rendering the app
 initSentry()
